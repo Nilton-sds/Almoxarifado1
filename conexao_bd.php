d633b26c32f469d718edba865774848c92315ea6
Nilton-SDS <107215706+Nilton-sds@users.noreply.github.com>
2026-08-30 08:35:56 -0300

    Update conexao_bd.php

--- conexao_bd.php
+++ conexao_bd.php
@@ -1,18 +1,15 @@
 <?php
+$servername = "dpg-daa0oogn74is739fpv70-a"; 
+$port       = "5432";
+$dbname     = "deploy_render_41hh";
+$username   = "admin";
+$password   = "9mmlcGMT6io2fCrnnYhBRehQ08evPvu0";
 
-
- 
-$servername = "localhost";
-$username = "root";
-$password = "";
-
-$conn = new PDO("mysql:host=$servername;dbname=restaurante_bd", $username, $password);
-// set the PDO error mode to exception
-$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-$consulta = $conn->prepare("SELECT codigo FROM usuario Where email=:email AND senha1=md5 (:senha)");
-
-
-
-
-
+try {
+    // Altera de mysql: para pgsql:
+    $conn = new PDO("pgsql:host=$servername;port=$port;dbname=$dbname", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e) {
+    echo "Erro ao consultar banco de dados: " . $e->getMessage();
+}
 ?>
