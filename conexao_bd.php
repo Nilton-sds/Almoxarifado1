fd182d40e6f555a1f38f15ef9ef73e2e9cd58508
Nilton-SDS <107215706+Nilton-sds@users.noreply.github.com>
2026-08-30 10:12:40 -0300

    Update conexao_bd.php

--- conexao_bd.php
+++ conexao_bd.php
@@ -1,15 +1,15 @@
 <?php
-$servername = "dpg-daa0oogn74is739fpv70-a"; 
+$servername = "dpg-daa0oogn74is739fpv70-a";
 $port       = "5432";
 $dbname     = "deploy_render_41hh";
 $username   = "admin";
 $password   = "9mmlcGMT6io2fCrnnYhBRehQ08evPvu0";
 
 try {
-    // Altera de mysql: para pgsql:
     $conn = new PDO("pgsql:host=$servername;port=$port;dbname=$dbname", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch(PDOException $e) {
-    echo "Erro ao consultar banco de dados: " . $e->getMessage();
+    // Interrompe o script para evitar o "Fatal error" na linha do prepare()
+    die("Erro ao consultar banco de dados: " . $e->getMessage());
 }
 ?>
