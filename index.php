98b8a13bf84ba51ffcf3b5bf9ceb13257bacab90
Nilton-SDS <107215706+Nilton-sds@users.noreply.github.com>
2026-09-01 10:13:52 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,55 +1,48 @@
-
 <?php
-   
-    if(isset($nome)){
-      echo"Bem-Vindo, $nome <br>";
-      echo"Essas informações precisa estar logado";
-    }else{
-      echo"Bem-Vindo, convidado <br>";
-      echo"Para acesso efetue o login";
-      echo"<br><a href='login.php'>Faça Login</a> Para ler o conteúdo";
-    }
+session_start();
+$nome = $_SESSION['usuario_nome'] ?? null;
+$erro = $_SESSION['erro_login'] ?? null;
+unset($_SESSION['erro_login']); // Limpa mensagem de erro após ler
 ?>
-
 <!DOCTYPE html>
-
-<html land="en">
+<html lang="pt-br">
 <head>
-    <!-- comentario -->
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Registro de Produtos 1.0</title>
-
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css">
 </head>
 <body>
-    <br>
-    <br>
-    <div class="container">
-    <h2>Efetue login </h2>
-    <br>
-    <form id="form_login" action="login.php" method="post">
+    <div class="container mt-4">
+        <?php if (isset($nome)): ?>
+            <div class="alert alert-success">
+                Bem-vindo, <strong><?php echo htmlspecialchars($nome); ?></strong>
+            </div>
+        <?php else: ?>
+            <div class="alert alert-info">
+                Bem-vindo, convidado.<br>
+                Para ter acesso total, efetue o login.
+            </div>
+        <?php endif; ?>
 
-        <?php if (isset($resultado) && $resultado["cod"]==0): ?>
-        <div class="alert alert-danger">
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-        <?php echo $resultado["msg"]; ?>
-        </div>
+        <h2>Efetue o login</h2>
+        <br>
 
-         
+        <?php if ($erro): ?>
+            <div class="alert alert-danger">
+                <?php echo htmlspecialchars($erro); ?>
+            </div>
+        <?php endif; ?>
 
-      <?php endif ; ?>
-
-    <input type="email"id="email" name="email" placeholder="Digite seu email" /><br><br>
-    <input type="password"id="senha1" name="senha1" placeholder="Digite sua senha" /><br><br>
-    <input type="submit"id="submeter " value="Entrar" class='btn btn-primary'/>
-
-</form>
+        <form id="form_login" action="login.php" method="post">
+            <div class="form-group">
+                <input type="email" id="email" name="email" class="form-control" placeholder="Digite seu e-mail" required />
+            </div>
+            <div class="form-group">
+                <input type="password" id="senha1" name="senha1" class="form-control" placeholder="Digite sua senha" required />
+            </div>
+            <input type="submit" id="submeter" value="Entrar" class="btn btn-primary" />
+        </form>
+    </div>
 </body>
-
-
-
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-
-
-
 </html>
