8dafe429ddbf19f6333d88fcc4bebd8bdcda2454
Nilton-SDS <107215706+Nilton-sds@users.noreply.github.com>
2026-09-01 10:02:08 -0300

    Update conexao_bd.php

--- conexao_bd.php
+++ conexao_bd.php
@@ -1,15 +1,16 @@
 <?php
-$servername = "dpg-daa0oogn74is739fpv70-a";
-$port       = "5432";
-$dbname     = "deploy_render_41hh";
-$username   = "admin";
-$password   = "9mmlcGMT6io2fCrnnYhBRehQ08evPvu0";
+// Configurações da conexão com o Render
+$host     = "dpg-daa0oogn74is739fpv70-a"; // Ou a External URL caso a aplicação rode fora do Render
+$db       = "deploy_render_41hh";
+$user     = "nilsantos"; // <-- Alterado de "admin" para "nilsantos"
+$password = "HxEoOGTN0JKIWJB7Nyz3uAw238Fgx0lF";
+$port     = "5432";
 
 try {
-    $conn = new PDO("pgsql:host=$servername;port=$port;dbname=$dbname", $username, $password);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch(PDOException $e) {
-    // Interrompe o script para evitar o "Fatal error" na linha do prepare()
+    $conn = new PDO("pgsql:host=$host;port=$port;dbname=$db;", $user, $password, [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+    ]);
+} catch (PDOException $e) {
     die("Erro ao consultar banco de dados: " . $e->getMessage());
 }
 ?>
